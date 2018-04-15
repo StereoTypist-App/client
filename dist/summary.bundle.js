bb3f7b0eb13b9ae72f402c2aa2419f10da48553f
Aung Kaung Myat <aungkaungmyat.olaf@gmail.com>
2018-04-15 16:58:34 -0400

    Set up donut chart

--- dist/summary.bundle.js
+++ dist/summary.bundle.js
@@ -10369,6 +10369,17 @@ const $ = require('jquery')
 
 $(document).ready(() => {
 
+    new Chartist.Pie('.ct-chart', {
+        labels: ['a', 'b', 'c', 'd'],
+        series: [20, 10, 35, 40]
+      }, {
+        donut: true,
+        donutWidth: 60,
+        donutSolid: true,
+        startAngle: 270,
+        showLabel: true
+      });
+      
 
     console.log('hi');
 })
