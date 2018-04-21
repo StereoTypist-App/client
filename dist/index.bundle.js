6608242545023b8d5a16f7aad428ac14ca206519
Vishnu Vijayan <vvijayan@purdue.edu>
2018-04-21 13:09:42 -0400

    Render open matches in a table

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -10971,11 +10971,35 @@ return jQuery;
 const $ = require('jquery')
 const Match = require('./models/match')
 
+function updateTable(tableData) {    
+    $('#games-tbody').empty()
+
+    if(!tableData) return
+
+    tableData.forEach((match) => {
+        const tr = $(`
+            <tr>
+                <td scope="row">
+                    <i class="fa fa-gamepad font-weight-bold ml-1"></i>
+                </td>
+                <td class="hidden-md-down">${match.id}</td>
+                <td>${match.count}</td>
+                <td>
+                    <a href="${"/game.html?match=" + match.id}" class="btn btn-sm btn-outline-indigo">JOIN</a>
+                </td>
+            </tr>
+        `)
+
+        tr.appendTo($('#games-tbody'))
+    })
+}
+
 $(document).ready(() => {
     const match = new Match()
 
     match.getMatches((matchData) => {
-        console.log(matchData)
+        console.log('Open matches: ', matchData)
+        updateTable(matchData)
     })
 })
 },{"./models/match":4,"jquery":2}],4:[function(require,module,exports){
